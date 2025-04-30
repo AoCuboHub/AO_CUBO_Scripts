27e0b7ec9a6abb427097ed26b30a8462ba6151b7
AoCuboHub <mandapramimisso@gmail.com>
2025-04-30 16:42:19 -0300

    Create scriptIndex.lua

---
+++ scriptIndex.lua
@@ -0,0 +1,18 @@
+return {
+    ESP = {
+        { name = "WallHack", url = "https://raw.githubusercontent.com/AoCuboHub/AO_CUBO_Scripts/main/ESP/WallHack.lua" },
+        { name = "XRay", url = "https://raw.githubusercontent.com/AoCuboHub/AO_CUBO_Scripts/main/ESP/XRay.lua" },
+    },
+    SHOP = {
+        { name = "AutoBuy", url = "https://raw.githubusercontent.com/AoCuboHub/AO_CUBO_Scripts/main/SHOP/AutoBuy.lua" },
+        { name = "AutoSell", url = "https://raw.githubusercontent.com/AoCuboHub/AO_CUBO_Scripts/main/SHOP/AutoSell.lua" },
+    },
+    AUTO = {
+        { name = "Farm Coins", url = "https://raw.githubusercontent.com/AoCuboHub/AO_CUBO_Scripts/main/AUTO/FarmCoins.lua" },
+        { name = "Auto Rebirth", url = "https://raw.githubusercontent.com/AoCuboHub/AO_CUBO_Scripts/main/AUTO/AutoRebirth.lua" },
+    },
+    MISC = {
+        { name = "Fly Mode", url = "https://raw.githubusercontent.com/AoCuboHub/AO_CUBO_Scripts/main/MISC/FlyMode.lua" },
+        { name = "NoClip", url = "https://raw.githubusercontent.com/AoCuboHub/AO_CUBO_Scripts/main/MISC/NoClip.lua" },
+    }
+}
